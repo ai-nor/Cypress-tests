5b45dec973408ee777824031d273010b697d8fa8
Tetyana Norynchak <tnorinchak@admixer.ua>
2023-02-16 08:59:51 +0000

    classwork

--- cypress/e2e/Hometask2/registr-authoriz.cy.js
+++ cypress/e2e/Hometask2/registr-authoriz.cy.js
@@ -1,8 +1,10 @@
 ///<reference types="cypress"/>
+import { faker } from '@faker-js/faker';
+import * as user from '../../fixtures/user.json';
 
-
-let userLogin;
-let userPassword;
+user.userName = faker.name.firstName();
+user.email = faker.internet.email();
+user.password = faker.internet.password(12);
 
 describe('Registration/authorization', () => {
 
@@ -11,16 +13,16 @@ describe('Registration/authorization', () => {
   })
 
   it(`Registration form`, () => {
+    cy.log('Перехід на реєстрацію');
     cy.get('#accountFrm_accountregister').click();
     cy.get(`[title="Continue"]`).click();
 
+    cy.log('**Заповнення форми**');
     cy.get('#AccountFrm_firstname').type('Tetiana')
     .should('have.value','Tetiana');
     cy.get('#AccountFrm_lastname').type('Nor')
     .should('have.value','Nor');
-    cy.get('#AccountFrm_email').type('nor' + Math.random() + '@test.com');
-    //cy.get('#AccountFrm_email').type('nor@test.com')
-    //.should('have.value','nor@test.com');
+    cy.get('#AccountFrm_email').type(user.email);
     cy.get('#AccountFrm_telephone').type('123456789')
     .should('have.value','123456789');
     cy.get('#AccountFrm_fax').type('987654321')
@@ -37,19 +39,11 @@ describe('Registration/authorization', () => {
     .should('have.value','3490');
     cy.get('#AccountFrm_postcode').type('BT472AY')
     .should('have.value','BT472AY');
-    cy.get('#AccountFrm_loginname').type('ai-nor' + + Math.random())
-    .invoke('text')  
-    .then(text => {
-     userLogin = text;
-     });
+    cy.get('#AccountFrm_loginname').type(user.userName);
     //cy.get('#AccountFrm_loginname').type('ai-nor');
-    cy.get('#AccountFrm_password').type('password123')
-    .invoke('text')  
-    .then(text => {
-     userPassword = text;
-     });
-    cy.get('#AccountFrm_confirm').type('password123')
-    .should('have.value','password123');
+    cy.get('#AccountFrm_password').type(user.password);
+    cy.get('#AccountFrm_confirm').type(user.password)
+    .should('have.value',user.password);
 
     cy.get('#AccountFrm_newsletter0').check();
     cy.get('#AccountFrm_agree').check();
@@ -70,8 +64,8 @@ describe('Registration/authorization', () => {
   it(`Authorization form`, () => {
     // cy.get('#loginFrm_loginname').type(`${userLogin}`);
     // cy.get(`#loginFrm_password`).type(`${userPassword}`);
-    cy.get('#loginFrm_loginname').type('ai-nor');
-    cy.get(`#loginFrm_password`).type('password123');
+    cy.get('#loginFrm_loginname').type(user.userName);
+    cy.get(`#loginFrm_password`).type(user.password);
     cy.get('[title="Login"]').click();
     cy.location().should((loc) => {
       expect(loc.href).to.eq('https://automationteststore.com/index.php?rt=account/account');
